98c4ffd60e9e8b1967c7dece5f7b9829311e6fd6
Brainnexx <154672574+Brainnexx@users.noreply.github.com>
2024-03-16 18:03:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['916289020180', 'Kartavya Kr.', true],
+  ['6281255369012', 'Ash', false], 
   [''] 
 ] //Number of owners
 
